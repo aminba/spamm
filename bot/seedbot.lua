37a36b3277b1a8da4dbf7e49828730263aba2617
aminba <elnabarekat@gmail.com>
2016-02-19 11:48:21 -0800

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {128132090,121167108,0,tonumber(our_id)},--Sudo users
+    sudo_users = {128132090,133202010,92112166,0,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[]],
